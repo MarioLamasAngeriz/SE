98c187adfa2af186d549611c5c5708b36d055ec1
MarioLamasAngeriz <m.lamasa@udc.es>
2025-09-16 10:56:11 +0200

    solo rojo

--- main.c
+++ main.c
@@ -42,9 +42,9 @@ int main(void) {
   led_red_init();
 
   while (1) {
-    led_green_toggle();
+    //led_green_toggle();
     delay();
-    //led_red_toggle();
+    led_red_toggle();
   }
 
   return 0;
