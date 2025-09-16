1c9e7fe428615c33182054158bc5b2e041d6d904
MarioLamasAngeriz <m.lamasa@udc.es>
2025-09-16 11:02:03 +0200

    creacion branch poc2

--- main.c
+++ main.c
@@ -20,10 +20,6 @@ void led_green_init() {
   GPIOD->PSOR |= (1 << 5);
 }
 
-void led_green_toggle() {
-  GPIOD->PTOR |= (1 << 5);
-}
-
 // LED_RED = PTE29
 void led_red_init() {
   SIM->COPC = 0;
@@ -33,18 +29,12 @@ void led_red_init() {
   GPIOE->PSOR |= (1 << 29);
 }
 
-void led_red_toggle(void) {
-  GPIOE->PTOR |= (1 << 29);
-}
-
 int main(void) {
   led_green_init();
   led_red_init();
 
   while (1) {
-    led_green_toggle();
-    delay();
-    led_red_toggle();
+    
   }
 
   return 0;
