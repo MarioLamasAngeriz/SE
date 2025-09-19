fd814901072215da2babc582e506cad899a4dd35
MarioLamasAngeriz <m.lamasa@udc.es>
2025-09-19 15:48:28 +0200

    inicializacion de los botones

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "MKL46Z4.h"
+#include "includes/MKL46Z4.h"
 
 // LED (RG)
 // LED_GREEN = PTD5
@@ -16,6 +16,7 @@ void led_green_init() {
   SIM->COPC = 0;
   SIM->SCGC5 |= SIM_SCGC5_PORTD(1);
   PORTD->PCR[5] |= PORT_PCR_MUX(1);
+  //PORTD->PCR[5] |= PORT_PCR_PE(1);
   GPIOD->PDDR |= (1 << 5); 
   GPIOD->PSOR |= (1 << 5);
 }
@@ -25,8 +26,29 @@ void led_red_init() {
   SIM->COPC = 0;
   SIM->SCGC5 |= SIM_SCGC5_PORTE(1);
   PORTE->PCR[29] |= PORT_PCR_MUX(1);
+  //PORTE->PCR[29] |= PORT_PCR_PE(1);
   GPIOE->PDDR |= (1 << 29);
   GPIOE->PSOR |= (1 << 29);
+}
+
+// SW1 = PTC3
+void button1_init(){
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTC(1);
+  PORTE->PCR[3] |= PORT_PCR_MUX(1);
+  PORTC->PCR[3] |= PORT_PCR_PE(1);
+  PORTC->PCR[3] |= PORT_PCR_PS(1);
+  GPIOC->PDDR &= ~(1 << 3);//aseguramos q es input
+}
+
+// SW3 = PTC12
+void button3_init(){ 
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTC(1);
+  PORTC->PCR[12] |= PORT_PCR_MUX(1);
+  PORTC->PCR[12] |= PORT_PCR_PE(1);
+  PORTC->PCR[12] |= PORT_PCR_PS(1); 
+  GPIOC->PDDR &= ~(1 << 12);//aseguramos q es input
 }
 
 int main(void) {
