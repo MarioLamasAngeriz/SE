d1b3701dc2b551a47a4d5a7f585b1c6f1dc0924e
MarioLamasAngeriz <m.lamasa@udc.es>
2025-10-17 00:30:07 +0200

    reestructuracion init botones y añadidas interrupciones y lógica de  encendido/apagado

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include "includes/MKL46Z4.h"
 #include "drivers/lcd.h"
+#include <stdint.h>
 
 #define SIZE 10
-// LED (RG)
-// LED_GREEN = PTD5
-// LED_RED = PTE29
 
-void irclk_ini(){
+volatile uint8_t sequence[SIZE] = {0, 1, 0, 1, 0, 0, 0, 0, 1, 1};
+volatile uint16_t indice = 0; //iteracion
+
+volatile uint8_t botonIzq = 0; //variables q deben actualizar as interrupcions
+volatile uint8_t botonDer = 0;
+
+volatile uint8_t botonPress = 0;
+volatile uint8_t end = 0;
+
+volatile uint16_t hit = 0;     //resultado
+volatile uint16_t misses = 0;
+
+void irclk_ini (void){
 	MCG->C1 = MCG_C1_IRCLKEN(1) | MCG_C1_IREFSTEN(1);
 	MCG->C2 = MCG_C2_IRCS(0); //0 32KHZ internal reference clock; 1= 4MHz irc
 }
@@ -16,12 +26,28 @@ void delay (void) {
 	for (i = 0; i < 1000000; i++);
 }
 
+void encender_led_green (void) {
+	GPIOD->PCOR = (1 << 5);
+}
+
+void encender_led_red (void) {
+	GPIOE->PCOR = (1 << 29);
+}
+
+void apagar_led_green (void) {
+	GPIOD->PSOR = (1 << 5);
+}
+
+void apagar_led_red (void) {
+	GPIOE->PSOR = (1 << 29);
+}
+
 // LED_GREEN = PTD5
 void led_green_init (void) {
 	SIM->SCGC5 |= SIM_SCGC5_PORTD(1);
 	PORTD->PCR[5] |= PORT_PCR_MUX(1);
 	GPIOD->PDDR |= (1 << 5); 
-	GPIOD->PSOR |= (1 << 5);
+	apagar_led_green();
 }
 
 // LED_RED = PTE29
@@ -29,87 +55,85 @@ void led_red_init (void) {
 	SIM->SCGC5 |= SIM_SCGC5_PORTE(1);
 	PORTE->PCR[29] |= PORT_PCR_MUX(1);
 	GPIOE->PDDR |= (1 << 29);
-	GPIOE->PSOR |= (1 << 29);
+	apagar_led_red();
 }
 
-void led_green_toggle (void) {
-	GPIOD->PTOR |= (1 << 5);
+void buttons_init(void) {
+	SIM->SCGC5 |= SIM_SCGC5_PORTC(1);
+
+	PORTC->PCR[3] |= PORT_PCR_MUX(1);
+	PORTC->PCR[3] |= PORT_PCR_PE(1);
+	PORTC->PCR[3] |= PORT_PCR_PS(1);
+	PORTC->PCR[3] |= PORT_PCR_IRQC(0x0A);
+	GPIOC->PDDR &= ~(1 << 3);
+				  
+	PORTC->PCR[12] |= PORT_PCR_MUX(1);
+	PORTC->PCR[12] |= PORT_PCR_PE(1);
+	PORTC->PCR[12] |= PORT_PCR_PS(1); 
+	PORTC->PCR[12] |= PORT_PCR_IRQC(0x0A);
+	GPIOC->PDDR &= ~(1 << 12);
+
+	NVIC_SetPriority(PORTC_PORTD_IRQn, 2);
+	NVIC_ClearPendingIRQ(PORTC_PORTD_IRQn);
+	NVIC_EnableIRQ(PORTC_PORTD_IRQn);
 }
 
-void led_red_toggle (void) {
-	GPIOE->PTOR |= (1 << 29);
+void PORTDIntHandler (void) {
+	if (!end && !botonPress) {
+		if (PORTC->ISFR & (1 << 3)) { //detecter flag de interrupcion 
+			botonPress = 1;
+			botonDer = 1;
+			PORTC->ISFR = (1 << 3);
+		}
+		if (PORTC->ISFR & (1 << 12)) {	//detectar flag de interrupcion 
+			botonPress = 1;
+			botonIzq = 1;
+			PORTC->ISFR = (1 << 12);
+		}
+	} else {
+		PORTC->ISFR = 0xFFFFFFFF; //borrando flags
+	}
 }
 
-int led_green_detect (void) {
-	return(GPIOD->PDIR & (1 << 5));
-}
+void ledsOnOffSec (void) {
 
-int led_red_detect (void) {
-	return(GPIOE->PDIR & (1 << 29));
-}
+	for (indice = 0; indice < SIZE; indice++){
 
-volatile uint8_t sequence[SIZE] = {0, 1, 0, 1, 0, 0, 0, 0, 1, 1};
-volatile uint16_t indice = 0; //iteracion
-
-volatile uint8_t botonIzq = 0; //variables q deben actualizar as interrupcions
-volatile uint8_t botonDer = 1;
-
-volatile uint16_t hit = 0;     //resultado
-volatile uint16_t misses = 0;
-
-void ledsOnOffSec (int led_green_state, int led_red_state) {
-
-	for (indice; indice <= SIZE; indice++){
-
-		led_green_state = led_green_detect();
-		led_red_state = led_red_detect();
-
-		if(!led_red_state) { led_red_toggle(); }
-		if(!led_green_state) { led_green_toggle(); }
-
+		apagar_led_green();
+		apagar_led_red();
+		
 		delay();
 
 		if (sequence[indice]) {
-			led_red_toggle(); 
+			encender_led_red();
 		} else {
-			led_green_toggle();
+			encender_led_green();
 		}
+	
+		botonPress = 0;
+		botonIzq = 0;
+		botonDer = 0;
 
-		if (botonIzq & !led_red_detect() || botonDer & !led_green_detect()){
-			hit++;
+		delay();
+	//	delay();
+
+		if (botonPress) {
+			if ((botonIzq && sequence[indice] == 1) || (botonDer && sequence[indice] == 0)) {
+				hit++;
+			} else {
+				misses++;
+			}
 		} else {
 			misses++;
 		}
 
-		delay();
-
+		apagar_led_green();
+		apagar_led_red();
+		
 		lcd_display_time(hit, misses);
+
+		delay();
 	}
-
-	led_green_state = led_green_detect();
-	led_red_state = led_red_detect();
-
-	if(!led_red_state) { led_red_toggle(); }
-	if(!led_green_state) { led_green_toggle(); }
-
-}
-
-// SW1 = PTC3
-void button1_init(void) {
-	SIM->SCGC5 |= SIM_SCGC5_PORTC(1);
-	PORTC->PCR[3] |= PORT_PCR_MUX(1);
-	PORTC->PCR[3] |= PORT_PCR_PE(1);
-	PORTC->PCR[3] |= PORT_PCR_PS(1);
-	GPIOC->PDDR &= ~(1 << 3); // aseguramos q es input
-}
-
-// SW3 = PTC12
-void button3_init(void) { 
-	SIM->SCGC5 |= SIM_SCGC5_PORTC(1);
-	PORTC->PCR[12] |= PORT_PCR_MUX(1);
-	PORTC->PCR[12] |= PORT_PCR_PE(1);
-	PORTC->PCR[12] |= PORT_PCR_PS(1); 
-	GPIOC->PDDR &= ~(1 << 12); // aseguramos q es input
 }
 
 //boton esquerdo led vermello ////////////////// boton dereito led verde
@@ -121,17 +145,19 @@ int main(void) {
 
 	led_green_init();
 	led_red_init();
+	buttons_init();
+	
+	lcd_display_time(hit, misses);
+	ledsOnOffSec();
 
-	int led_green_state = 0; //declarar mais arriba e usar para comprobar interrupcions? ou innecesario?
-	int led_red_state = 0;
-
-	ledsOnOffSec(led_green_state, led_red_state);
+	end = 1;
 
 	while(1){
+		lcd_display_time(hit, misses);
 		delay();
-		lcd_display_time(0, 0);
+		LCD->GCR &= ~LCD_GCR_LCDEN_MASK;
 		delay();
-		lcd_display_time(hit, misses); //falta parpadeo
+		LCD->GCR |= LCD_GCR_LCDEN_MASK;   
 		delay();
 	}
 }
